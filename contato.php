8ee5726cd3261df6b28458351472ee899f2b743a
Andreina Oliveira <51168329+andreinaoliveira@users.noreply.github.com>
2020-04-30 17:52:45 -0300

    Add files via upload

---
+++ contato.php
@@ -0,0 +1,16 @@
+<?php include 'header.php';?>
+<div id="allDiv" style="background-image: url(img/africa.jpg);  background-size: 100%">
+			<div id="contato">
+				<h1>Contato</h1>
+				<p>Email: andreinaoliveira@gmail.com<br>
+				Telefone: (92) 9999-9999<br>
+				site: www.agenciaviangem.com.br
+				</p>
+
+				<br><br>
+
+				<p>Turma: CIN01S1<br>
+				Aluna: Andreina Oliveira<br>
+				Professora: Jéssica Oliveira</p>
+			</div>
+<?php include 'footer.php';?>
---
+++ footer.php
@@ -0,0 +1,6 @@
+		</div>
+		<footer>
+			<p class="container">Desenvolvido por Andreina Oliveira e Italo Amadeus</p>
+		</footer>
+	</body>
+</html>
