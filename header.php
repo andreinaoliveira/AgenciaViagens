6d8a4d94d0d83c3fff00ae307255190240313e61
Italo Amadeus <42629870+itkb12@users.noreply.github.com>
2020-06-04 20:56:01 -0400

    Adicionei um botão de Logar e um de Sair

--- header.php
+++ header.php
@@ -13,9 +13,18 @@
 					<a href="index.php">Home</a>
 					<a href="promocoes.php">Promoções</a>
 					<a href="contato.php">Fale conosco</a>
-					<a href="login.php">
-					<img border="0" alt="Login" src="img/logar.png" height="40px">
-				</a>
+					<?php
+					$conexao = new mysqli("localhost","root","","azulviagens") or die("Erro na conexão com o banco de dados");
+					session_start();
+						if((!isset ($_SESSION['email']) == true) and (!isset ($_SESSION['senha']) == true))
+						{
+							echo '<a href="login.php"><img border="0" alt="Login" src="img/logar.png" height="40px"></a>';
+						}else{
+							$logado = $_SESSION['email'];
+							echo '<a href="deslogar.php">Sair</a>';
+						}
+						
+					?>
 				</nav>
 			</div>
 		</header>
