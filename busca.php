d90c301e249648e8eac2de52e9012f0bfd06e75b
Italo Amadeus <42629870+itkb12@users.noreply.github.com>
2020-06-05 04:07:08 -0400

    MUDEI TUDO
    Agora, se tiver 3 passagens, vão aparecer as últimas 3, se não tiver nenhuma, vai ficar vazio, se tiver 2, vai aparecer 2

--- busca.php
+++ busca.php
@@ -1,123 +1,46 @@
 <?php include 'header.php';?>
 
 <div id="allDiv">
-	<form action="check-in.php" method="get">
-		<div id="voo1">
-			<img src="img/voo.PNG">
+	<form action="check-in.php" method="post">
 			<?php
-				$origem = $_POST['origem'];
-				$destino = $_POST['destino'];
-
-				$sql_origem = "SELECT id,cidade,codigo_aita FROM aeroporto WHERE id = '$origem'";
-				$sql_destino = "SELECT id,cidade,codigo_aita FROM aeroporto WHERE id = '$destino'";
-
-				$sql_voo = "SELECT id,horario_partida,horario_chegada FROM voo WHERE id_origem = '$origem' AND id_destino = '$destino'";
-
-				$query= mysqli_query($conexao,$sql_voo);
-				$linhas= mysqli_affected_rows($conexao);
-
-				if ($linhas>0) {
-					while($exibirNome=mysqli_fetch_array($query)) {
+			$cont = 1;
 			
-					$id_voo= $exibirNome[0];
-					$horario_partida= $exibirNome[1];
-					$horario_chegada= $exibirNome[2];
+			$origem = $_POST['origem'];
+			$destino = $_POST['destino'];
 			
-					}
-				}else{
-					//colocar JavaScript de aviso que não tem aqui!
-					header("Location:teste.php");
-				}
+			$sql = "SELECT voo.id,voo.horario_partida,voo.horario_chegada,
+			a.cidade AS origem ,a.codigo_aita AS origem_aita,b.cidade AS destino,b.codigo_aita AS destino_aita
+			FROM voo JOIN aeroporto a ON voo.id_origem = a.id JOIN aeroporto b ON voo.id_destino = b.id
+			WHERE voo.id_origem = '$origem' AND voo.id_destino = '$destino' ORDER BY id DESC LIMIT 0,3";
 
-				$query= mysqli_query($conexao,$sql_origem);
-				$linhas= mysqli_affected_rows($conexao);
+			$query= mysqli_query($conexao,$sql);
 
-				if ($linhas>0) {
-					while($exibirNome=mysqli_fetch_array($query)) {
+			while ($row = $query->fetch_assoc()){
+				echo '	<div id="voo'.$cont.'">
+							<img src="img/voo.PNG">
+							<p id="vooTitulo">'. $row['origem_aita'] .' - '. $row['destino_aita'] .'</p>
+
+							<div id="vooOrigem">
+								<p>'. $row['origem'] .'</p>
+								<p>'. $row['horario_partida'] .'</p>
+							</div>
 			
-					$id_origem= $exibirNome[0];
-					$cidade_origem= $exibirNome[1];
-					$aita_origem= $exibirNome[2];
-			
-					}
-				}
+							<div id="vooDestino">		
+								<p>'. $row['destino'] .'</p>
+								<p>'. $row['horario_chegada'] .'</p>
+							</div>
 
-				$query= mysqli_query($conexao,$sql_destino);
-				$linhas= mysqli_affected_rows($conexao);
+							<div id="vooSel">
+								<center>
+									<input type="radio" name="ida" id="sim" value="'. $row['id'] .'">
+									<label for="sim">Selecionar Passagem</label>
+								</center>
+							</div>
 
-				if ($linhas>0) {
-					while($exibirNome=mysqli_fetch_array($query)) {
-			
-					$id_destino= $exibirNome[0];
-					$cidade_destino= $exibirNome[1];
-					$aita_destino= $exibirNome[2];
-			
-					}
-				}
-				echo'<p id="vooTitulo"> '. $aita_origem .' - '. $aita_destino .'</p>';
-			?>
-			<div id="vooOrigem">
-				<p><?php echo $cidade_origem; ?></p>
-				<p><?php echo $horario_partida; ?></p>
-			</div>
-			
-			<div id="vooDestino">		
-				<p><?php echo $cidade_destino ?></p>
-				<p><?php echo $horario_chegada; ?></p>
-			</div>
-			<div id="vooSel">
-				<center>
-					<input type="radio" name="ida" id="sim" value="sim">
-					<label for="sim">Selecionar Passagem</label>
-				</center>
-			</div>
-		</div>
-
-		<div id="voo2">
-			<img src="img/voo.PNG">
-			<p id="vooTitulo">SAO - MAO</p>
-
-			<div id="vooOrigem">
-				<p>São Paulo</p>
-				<p>23:00</p>
-			</div>
-			
-			<div id="vooDestino">		
-				<p>Manaus</p>
-				<p>00:00</p>
-			</div>
-
-			<div id="vooSel">
-				<center>
-					<input type="radio" name="ida" id="sim" value="sim">
-					<label for="sim">Selecionar Passagem</label>
-				</center>
-			</div>
-
-		</div>
-
-		<div id="voo3">
-			<img src="img/voo.PNG">
-			<p id="vooTitulo">SAO - MAO</p>
-
-			<div id="vooOrigem">
-				<p>São Paulo</p>
-				<p>23:00</p>
-			</div>
-			
-			<div id="vooDestino">		
-				<p>Manaus</p>
-				<p>00:00</p>
-			</div>
-
-			<div id="vooSel">
-				<center>
-					<input type="radio" name="ida" id="sim" value="sim">
-					<label for="sim">Selecionar Passagem</label>
-				</center>
-			</div>
-
-		</div>
+						</div>';
+				$cont=$cont+1;
+			}
+		?>
 
 		<div id="vooComprar">
 			<center>
