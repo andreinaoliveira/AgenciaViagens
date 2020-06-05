be309c53ffe17f46447998ca28e823fe8c2bb3df
Andreina Oliveira <51168329+andreinaoliveira@users.noreply.github.com>
2020-06-05 18:50:08 -0300

    tela de check-in incluida

--- check-in.php
+++ check-in.php
@@ -6,7 +6,18 @@
 		echo 'Escolha alguma';
 	}else{
 		$id_voo = $_POST['ida'];
-	//ANDREINA FAZER A TELA DE CHECK-IN
+	<div id="allDiv">
+		<div id="checkCorpo">
+			<img src="img/check-in.png">
+			<div id="checkNome"><p>Andreina Oliveira</p></div>
+			<div id="checkCpf"><p>123.456.789-10</p></div>
+			<div id="checkOrigem"><p>Cidade de Origem</p></div>
+			<div id="checkDestino"><p>Cidade de Destino</p></div>
+			<div id="checkOrigemData"><p>Data/Hora</p></div>
+			<div id="checkDestinoData"><p>Data/Hora</p></div>
+			<div id="checkValor">R$ 0,00</div>
+		</div>
+	</div>
 	}
 
 ?>
