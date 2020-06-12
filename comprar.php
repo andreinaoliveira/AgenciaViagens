ceeaf09c47621a2d9339bd6a4a0b76404f51f761
Victormaia98 <49289997+Victormaia98@users.noreply.github.com>
2020-06-11 23:17:49 -0400

    Update comprar.php

--- comprar.php
+++ comprar.php
@@ -1,17 +1,4 @@
-<?php include 'header.php';?>
-<?php
-$cliente = $_SESSION['codigo'];
-$voo = $_POST['voo'];
-$qtd = $_POST['qtd'];
-$preco = $qtd*300.00;
-$sql = "INSERT INTO passagem (preco,quantidade,id_cliente,id_voo) VALUES ($preco,$qtd,$cliente,$voo)";
-$conexao->query($sql) or die("Codigo já existente!<br><br><br> <input type='button' value='Voltar' onclick=location='index.php'>");
-echo 'Compra de '.$preco.' realizada!';
-$sql_voo = "SELECT id_aviao FROM voo WHERE id = $voo";
-$query = mysqli_query($conexao,$sql_voo);
-	while ($row = $query->fetch_assoc()){
-		$aviao = $row['id_aviao'];
-	}
-$sql_aviao = "UPDATE  aviao SET vagas = vagas - $qtd WHERE id=$aviao";
-$conexao->query($sql_aviao) or die("Codigo já existente!<br><br><br> <input type='button' value='Voltar' onclick=location='index.php'>");
-?>
+<script>
+alert('Compra realizada com sucesso!')
+window.location.href = 'index.php';
+</script>
