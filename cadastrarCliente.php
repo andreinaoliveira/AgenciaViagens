a24104d61ec83fbca871ff534c2c64801f1c1eec
Victormaia98 <49289997+Victormaia98@users.noreply.github.com>
2020-06-11 21:43:33 -0400

    Update cadastrarCliente.php

--- cadastrarCliente.php
+++ cadastrarCliente.php
@@ -1,19 +1,32 @@
 <?php
-$conexao = new mysqli("localhost","root","","azulviagens") or die("Erro na conexão com o banco de dados");
+	 if(isset($_POST['button'])) { 
 
-$nome = $_POST['nome'];
-$sobrenome = $_POST['sobrenome'];
-$nomecliente = $nome . ' ' . $sobrenome;
-$cpfcliente = $_POST['cpf'];
-$data_nascimentocliente = $_POST['nascimento'];
-$emailcliente = $_POST['email'];
-$senhacliente = $_POST['senha'];
+		$conexao = new mysqli("localhost","root","","azulviagens") or die("Erro na conexão com o banco de dados");
 
-$sql = "INSERT INTO cliente (nome,cpf,data_nascimento,email,senha) VALUES ('$nomecliente','$cpfcliente',
-'$data_nascimentocliente','$emailcliente','$senhacliente')";
-$conexao->query($sql) or die("Codigo já existente!<br><br><br> <input type='button' value='Voltar' onclick=location='login.php'>");
-echo "<center>";
-echo "<h1>Cadastro realizado!</h1><br>";
-echo "<input type='button' onclick=location='index.php' value='Voltar à Tela de Interface'>";
-echo "</center>";
+		$nome = $_POST['nome'];
+		$sobrenome = $_POST['sobrenome'];
+		$nomecliente = $nome . ' ' . $sobrenome;
+		$cpfcliente = $_POST['cpf'];
+		$data_nascimentocliente = $_POST['nascimento'];
+		$emailcliente = $_POST['email'];
+		$senhacliente = $_POST['senha'];
+		
+		
+		$sql = "SELECT * FROM cliente WHERE cpf = $cpfcliente";
+        $result = $conexao->query($sql) or die("lalalal");
+        
+		if (!$result->num_rows > 0) {
+			$sql = "INSERT INTO cliente (nome,cpf,data_nascimento,email,senha) VALUES ('$nomecliente','$cpfcliente',
+			'$data_nascimentocliente','$emailcliente','$senhacliente')";
+			$conexao->query($sql) or die("Codigo já existente!<br><br><br> <input type='button' value='Voltar' onclick=location='login.php'>");
+			echo "<center>";
+			echo "<script> alert('Cadastro Realizado com Sucesso!'); </script>";
+			echo "</center>";
+		} else {
+            echo "<center>";
+			echo "<script> alert('Dados Replicados!'); </script>";
+			echo "</center>";
+		}
+	}
+
 ?>
